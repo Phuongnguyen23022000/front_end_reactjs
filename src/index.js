9d416fe3403c04fc31b8193932b4b0142bfb8abc
Nguuyenvanphuong <Nguyenvanphuong.23032000@gmail.com>
2023-03-28 11:04:33 +0700

    setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
